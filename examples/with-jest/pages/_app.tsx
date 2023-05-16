f1b0c05515f48b657b955da0a8aa05714f1f762c
Pascal Wegner <pascal.wegner@edith.care>
2023-05-16 15:32:11 +0200

    Ignore file

--- examples/with-jest/pages/_app.tsx
+++ examples/with-jest/pages/_app.tsx
@@ -1,7 +1,7 @@
+/* istanbul ignore file */
 import '@/styles/global.css'
 import type { AppProps } from 'next/app'
 
-/* istanbul ignore next */
 export default function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
